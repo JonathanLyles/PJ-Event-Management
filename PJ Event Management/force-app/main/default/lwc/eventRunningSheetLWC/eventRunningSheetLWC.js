f135bd81bf6e7f2c7e03d8fb1b4065082282cf9b
john38291 <162273614+john38291@users.noreply.github.com>
2024-09-07 22:38:07 -0400

    Added JS File Hint

--- PJ Event Management/force-app/main/default/lwc/eventRunningSheetLWC/eventRunningSheetLWC.js
+++ PJ Event Management/force-app/main/default/lwc/eventRunningSheetLWC/eventRunningSheetLWC.js
@@ -1,3 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
+import getEventDetails from '@salesforce/apex/EventRunningSheetController.getEventDetails';
+import getTasks from '@salesforce/apex/EventRunningSheetController.getTasks';
 
-export default class EventRunningSheetLWC extends LightningElement {}
+export default class EventRunningSheetLWC extends LightningElement {
+    eventName;
+    eventDate;
+    @api recordId;
+    taskList = [];
+
+    // Load event details and tasks using wire service
+    @wire(getEventDetails, {recordId: '$recordId'})
+    wiredEventDetails({ error, data }) {
+        if (data) {
+            // set this.eventName using Event_Name__c
+            // this.eventDate using Event_Date__c;
+        }
+    }
+
+    @wire(getTasks, {recordId: '$recordId'})
+    wiredTasks({ error, data }) {
+        if (data) {
+            //set this.taskList using data
+        }
+    }
+}
